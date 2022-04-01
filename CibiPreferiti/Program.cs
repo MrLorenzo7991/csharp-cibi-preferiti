857bcc26dd8f4c7ea7e614f2b9c225684b0bf305
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-01 14:27:03 +0200

    Completato esercizio

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//array cibi preferiti
+string[] cibiPreferiti = {"Lasagne", "Pizza", "Carne alla brace", "Biscotti", "Fagioli", "Ravioli cinesi", "Tiramisù"};
+
+//Lunghezza classifica
+Console.WriteLine("La lunghezza della classifica è di " + cibiPreferiti.Length + " cibi.");
+
+//Stampa classifica
+Console.WriteLine("I cibi presenti nella lista sono: ");
+for(int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
+
+//Cibo top
+Console.WriteLine("Il cibo primo in classifica è: " + cibiPreferiti[0]);
+
+//Cibo ultimo
+Console.WriteLine("Il cibo ultimo in classifica è: " + cibiPreferiti[cibiPreferiti.Length - 1]);
